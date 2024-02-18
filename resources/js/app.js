9ad973e0ddaaec3c8695efdcc294f600e71072e6
Elvis  Yerel Roman Concha <yerel9212@yahoo.es>
2024-02-18 16:25:33 -0500

    Updated Login

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,26 +5,43 @@ import { router } from "./config/rutes";
 import { components } from "./config/globalComponents";
 
 import App from "./App.vue";
+import Login from "./Login/Login.vue";
+
+//bootstrap
 import * as bootstrap from "bootstrap";
 
+//Starting App
 $server
     .get("/api/gateway/user")
     .then((res) => {
+        //Start authenticable user
         window.$auth = res.data;
 
+        if (window.location.pathname == "/login") {
+            window.location.href = process.env.MIX_APP_URL;
+        }
+
+        //init app
         const app = createApp(App);
 
+        //global properties
         app.config.globalProperties.$server = $server;
         app.config.globalProperties.$host = $host;
         app.config.globalProperties.$echo = $echo;
         app.config.globalProperties.$channels = $channels;
 
+        //global components
         components.forEach((index) => {
             app.component(index[0], index[1]);
         });
 
+        //use vue router
         app.use(router);
 
         app.mount("#app");
     })
-    .catch((err) => {});
+    .catch((err) => {
+        //Unauthenticable APP
+        const app = createApp(Login);
+        app.mount("#login");
+    });
--- resources/views/auth.blade.php
+++ resources/views/auth.blade.php
@@ -1,39 +1,5 @@
-<!DOCTYPE html>
-<html>
+@extends('layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="icon" href="favicon.png" type="image/png">
-
-
-    <!-- Fonts -->
-    <link rel="stylesheet"
-        href="{{ app()->environment('production') ? secure_asset('css/app.css') : asset('css/app.css') }}">
-
-    <script src="{{ app()->environment('production') ? secure_asset('js/app.js') : asset('js/app.js') }}" defer></script>
-
-
-</head>
-
-<body class="font-sans antialiased">
-
-    <div class="container">
-        <div class="card mt-5">
-            <div class="card-head mb-2 text-center fw-bold h1 text-secondary">
-                Login
-            </div>
-            <div class="card-body text-center text-capitalize mt-5">
-                <form action="/redirect" method="GET">
-                    <button type="submit" class=" btn px-4 py-2 btn-primary">
-                        Login with Astian
-                    </button>
-                </form>
-            </div>
-        </div>
-    </div>
-</body>
-
-</html>
+@section('content')
+     <div id="login"></div>
+@endsection
