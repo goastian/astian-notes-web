afe42555957583b42530444b6f4531466b0c2ad6
Elvis  Yerel Roman Concha <yerel9212@yahoo.es>
2024-03-22 01:24:38 -0500

    Added new route

--- resources/js/config/rutes.js
+++ resources/js/config/rutes.js
@@ -1,22 +1,29 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import VNotes from "../Pages/Index.vue";
-import VNotesCreate from "../Pages/Editor.vue";
-import VTagCreate from "../Pages/Tag.vue";
-
 const routes = [
-    { path: "/:id?", name: "notes", component: VNotes, meta: { auth: true } },
+    {
+        path: "/login",
+        name: "login",
+        component: () => import("../Login/Login.vue"),
+        meta: { auth: false },
+    },
+    {
+        path: "/:id?",
+        name: "notes",
+        component: () => import("../Pages/Index.vue"),
+        meta: { auth: true },
+    },
     {
         path: "/notes/:id?",
         name: "notes.create",
-        component: VNotesCreate,
-        meta: { auth: true }
+        component: () => import("../Pages/Editor.vue"),
+        meta: { auth: true },
     },
     {
         path: "/tags",
         name: "tag.create",
-        component: VTagCreate,
-        meta: { auth: true }
+        component: () => import("../Pages/Tag.vue"),
+        meta: { auth: true },
     },
 ];
 
